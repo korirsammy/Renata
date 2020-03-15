5134ce508199732e74621553538dd010a2618364
Korir <sammy.korir@bell.corp.bce.ca>
2020-03-15 20:36:56 -0300

    Added client-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,7 +21,7 @@ namespace API
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -39,13 +39,15 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+           services.AddMvc(option => option.EnableEndpointRouting = false)
+           .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-             if (env.IsDevelopment())
+           if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -58,6 +60,7 @@ namespace API
             // app.UseHttpsRedirection();
             app.UseCors("CorsPolicy");
             app.UseMvc();
+           
         }
     }
 }
